4fd7ef533a51b0beafe319d2964de65198af8d48
macbook <macbook@macdeMacBook-Pro.local>
2020-10-01 15:57:56 +0800

    use flyway migrate & config integration

---
+++ src/test/java/com/github/springbootdemo/demo/integration/MyIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.github.springbootdemo.demo.integration;
+
+import org.junit.jupiter.api.Test;
+
+public class MyIntegrationTest {
+    // 集成测试
+
+    @Test
+    public void myIntegrationTest(){
+
+    }
+}
